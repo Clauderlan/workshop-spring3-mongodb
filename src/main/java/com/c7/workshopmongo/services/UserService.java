61f175e9cf839829c2cbfbf9d90170dba471dbf2
Clauderlan <clauderlanbatista@hotmail.com>
2023-05-13 16:19:52 -0300

    User insert

--- src/main/java/com/c7/workshopmongo/services/UserService.java
+++ src/main/java/com/c7/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.c7.workshopmongo.services;
 
 import com.c7.workshopmongo.domain.User;
+import com.c7.workshopmongo.dto.UserDTO;
 import com.c7.workshopmongo.repositories.UserRepository;
 import com.c7.workshopmongo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,4 +20,13 @@ public class UserService {
     public User findById(String id){
         return userRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException(id));
     }
+
+    public User insert(User user){
+        return userRepository.insert(user);
+    }
+
+    public User fromDTO(UserDTO userDTO){
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
+    }
+
 }
