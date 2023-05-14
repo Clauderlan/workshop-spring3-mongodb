985017855bb3dcda99a18764f0ca8010bcc04d95
Clauderlan <clauderlanbatista@hotmail.com>
2023-05-13 22:11:04 -0300

    User findPosts

--- src/main/java/com/c7/workshopmongo/resources/UserResource.java
+++ src/main/java/com/c7/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.c7.workshopmongo.resources;
 
+import com.c7.workshopmongo.domain.Post;
 import com.c7.workshopmongo.domain.User;
 import com.c7.workshopmongo.dto.UserDTO;
 import com.c7.workshopmongo.services.UserService;
@@ -9,9 +10,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 @RestController
 @RequestMapping(value = "/users")
@@ -31,6 +31,12 @@ public class UserResource {
         User user = userService.findById(id);
         return ResponseEntity.ok().body(new UserDTO(user));
     }
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findByIdPosts(@PathVariable String id){
+        User user = userService.findById(id);
+        List<Post> posts = user.getPosts();
+        return ResponseEntity.ok().body(posts);
+    }
     @PostMapping
     public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO){
         User user = userService.fromDTO(userDTO);
@@ -38,7 +44,6 @@ public class UserResource {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
         return ResponseEntity.created(uri).build();
     }
-
     @PutMapping(value = "/{id}")
     public ResponseEntity<Void> update(@PathVariable String id, @RequestBody UserDTO user){
         userService.update(id,userService.fromDTO(user));
