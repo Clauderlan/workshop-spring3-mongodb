7c13d8642510856f1aa984ab36a1e6a5a34d86a4
Clauderlan <clauderlanbatista@hotmail.com>
2023-05-13 22:16:33 -0300

    Post findById

---
+++ src/main/java/com/c7/workshopmongo/services/PostService.java
@@ -0,0 +1,19 @@
+package com.c7.workshopmongo.services;
+
+import com.c7.workshopmongo.domain.Post;
+import com.c7.workshopmongo.repositories.PostRepository;
+import com.c7.workshopmongo.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository postRepository;
+
+    public Post findById(String id){
+        return postRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException(id));
+    }
+    
+}
